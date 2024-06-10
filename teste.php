5078c20d2cf2f8c01c1b8414f9d79c21003890ce
rafaelgutierres049 <98500864+rafaelgutierres049@users.noreply.github.com>
2024-06-10 10:24:55 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,14 +1,1 @@
-<?php
-session_start(); // Inicia a sessão se ainda não estiver iniciada
 
-// Verifica se o usuário está logado
-if(isset($_SESSION['id_usuario'])) {
-    // O usuário está logado, então podemos exibir o ID do usuário
-    $id_usuario = $_SESSION['id_usuario'];
-    $nome = $_SESSION['nome_usuario'];
-    echo "O ID do usuário é: $id_usuario e seu nome é: $nome";
-} else {
-    // Se o usuário não estiver logado, você pode exibir uma mensagem de erro ou redirecionar para a página de login
-    echo "O usuário não está logado.";
-}
-?>
